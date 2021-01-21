4238db88988aa2d10bc32f73a0d408993d0e447e
hepeng01 <hepeng01@shiguangjk.com>
2021-01-21 15:29:19 +0800

    api

--- api/handler/dslrun.go
+++ api/handler/dslrun.go
@@ -6,12 +6,9 @@ import (
 	"github.com/skyhackvip/risk_engine/global"
 	"github.com/skyhackvip/risk_engine/internal/dto"
 	"net/http"
-	//"strconv"
-	"fmt"
 )
 
 func init() {
-	fmt.Println("init")
 	global.Features = dto.NewGlobalFeatures()
 	global.DslResult = dto.NewDslResult()
 }
@@ -20,19 +17,18 @@ func DslRunHandler(c *gin.Context) {
 	var request dto.DslRunRequest
 	err := c.ShouldBindJSON(&request)
 	if err != nil {
-		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
 	}
-	//global.Features = &dto.GlobalFeatureS{Features: make(map[string]dto.Feature)}
 	for k, v := range request.Features {
-		//datasource.SetFeature(k, v)
 		f := dto.Feature{Name: k, Value: v}
 		global.Features.Set(f)
 	}
 
-	fmt.Println(global.Features)
-
 	flow := request.Flow
-	dsl := dslparser.LoadDslFromFile("yaml/" + flow + ".yaml")
+	dsl := dslparser.LoadDslFromFile("test/yaml/" + flow + ".yaml")
 	rs := dsl.Parse(global.DslResult)
 	c.JSON(http.StatusOK, gin.H{
 		"flow":   flow,
